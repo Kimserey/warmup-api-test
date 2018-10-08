15ba090647b2db4ba9efec947973306c05827b0a
Kimserey <kimserey.lam@groomgy.com>
2018-10-08 21:37:19 +0100

    project

---
+++ WarmupApiTest/PersonsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarmupApiTest
+{
+    [Route("api/persons")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string[]> GetAll()
+        {
+            return new[] { "Kim", "Tom" };
+        }
+
+        [HttpGet("kim")]
+        public ActionResult<string> GetKim()
+        {
+            return "Kimserey";
+        }
+    }
+}
---
+++ WarmupApiTest/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarmupApiTest
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string[]> GetAll()
+        {
+            return new[] { "Value 1", "Value 2" };
+        }
+
+        [HttpGet("one")]
+        public ActionResult<string> GetOne()
+        {
+            return "Value 1";
+        }
+    }
+}
