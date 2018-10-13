dc59f20cca37f760df2899fbd87d30ec0456759c
Kimserey <kimserey.lam@groomgy.com>
2018-10-13 19:00:17 +0100

    warmup script

--- warmup/wait.sh
+++ warmup/wait.sh
@@ -1,4 +1,1 @@
 #!/bin/sh
-
-./wait-for.sh --timeout=60 localhost:5100 -- curl -sS http://localhost:5100/api/
-./wait-for.sh --timeout=60 localhost:5000 -- curl -sS http://localhost:5000/api/persons
--- warmup/warmup.sh
+++ warmup/warmup.sh
@@ -1,2 +1,25 @@
-curl -sS http://warmupapitest/api/values
-curl -sS http://warmupapitest/api/persons
+#!/bin/bash
+
+TIMEOUT=60
+
+warmup() {
+  for i in `seq $TIMEOUT`; do
+    nc -zv $1 $2 2>&1
+    result=$?
+
+    if [ $result -eq 0 ]; then
+      echo "Warmup [$3] response: $(curl -sS $3)"
+      return 0
+    fi
+    sleep 1
+  done
+
+  echo "Warmup timed out for API '$1:$2'" >&2
+  return 1
+}
+
+warmup $1 $2 $3
+
+# Examples:
+# ./warmup.sh localhost 5100 http://localhost:5100/api/test
+# ./warmup.sh localhost 5000 http://localhost:5000/api/persons
